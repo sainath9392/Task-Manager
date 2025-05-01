fcfe37538578a2e22db0dc58d7704a7e197d9425
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-01 17:30:32 +0530

    todo checklist update for user route done

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -221,6 +221,42 @@ const updateTaskStatus = async (req, res) => {
 //acces Private
 const updateTaskChecklist = async (req, res) => {
   try {
+    const { todoChecklist } = req.body;
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    if (!task.assignedTo.includes(req.user._id) && req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to update checklist" });
+    }
+
+    task.todoChecklist = todoChecklist; //replace with updated checklist
+
+    //auto update progress based on checklist completed
+    const completedCount = task.todoChecklist.filter(
+      (item) => item.completed
+    ).length;
+    const totalItems = task.todoChecklist.length;
+
+    task.progress =
+      totalItems > 0 ? Math.round((completedCount / totalItems) * 100) : 0;
+
+    if (task.progress === 100) {
+      task.status = "Completed";
+    } else if (task.progress > 0) {
+      task.status = "In Progress";
+    } else {
+      task.status = "Pending";
+    }
+
+    await task.save();
+    const updatedTask = await Task
+      .findById(req.params.id)
+      .populate("assignedTo", "name email profileImageUrl");
+
+    res.json({ message: "Task checklist updated", task: updatedTask });
   } catch (error) {
     res.status(500).json({ message: "server error", error: error.message });
   }
