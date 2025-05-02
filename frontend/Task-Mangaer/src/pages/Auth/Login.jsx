253a11d44f32f4e518968ee6206729498a4b6d07
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-03 00:40:17 +0530

    feat: add Input component with password visibility toggle and AuthLayout for authentication pages

--- frontend/Task-Mangaer/src/pages/Auth/Login.jsx
+++ frontend/Task-Mangaer/src/pages/Auth/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import AuthLayout from "../../components/Layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/inputs/input";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const navigate = useNavigate();
+
+  //Handle login form submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-state-700 mt-[5px] mb-6">
+          Please enter your details to logi in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="your@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 characters"
+            type="password"
+          />
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
