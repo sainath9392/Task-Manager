36c62a01811ab0db954210bfbbaafca4044bd8a8
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-05 21:55:03 +0530

    fix: add early return in Modal component and enhance styling for SelectUsers component buttons

--- frontend/Task-Mangaer/src/components/Modal.jsx
+++ frontend/Task-Mangaer/src/components/Modal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Modal = ({ children, isOpen, onClose, title }) => {
+    if(!isOpen) return;
   return (
     <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50">
       <div className="relative p-4 w-full max-w-2xl max-h-full">
