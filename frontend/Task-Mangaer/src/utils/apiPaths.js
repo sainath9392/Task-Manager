8d8eeade6e4a6d4844f19da0eb92edb4eaf9ab14
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-09 01:06:53 +0530

    fix: update BASE_URL to point to the production server

--- frontend/Task-Mangaer/src/utils/apiPaths.js
+++ frontend/Task-Mangaer/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+
+export const BASE_URL = "https://task-manager-ya1t.onrender.com"; // Replace with your server URL
 
 //utils/apiPaths.js
 export const API_PATHS = {
