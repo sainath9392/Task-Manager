ccba69584d52fb9b05f175d3e94325c903b0095b
sainath9392 <sainathduvvuri03@gmail.com>
2025-05-09 11:58:39 +0530

    updated eslint

--- frontend/Task-Manager/eslint.config.js
+++ frontend/Task-Manager/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
 
 export default [
   { ignores: ['dist'] },
@@ -19,6 +20,7 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -28,6 +30,16 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'import/no-unresolved': 'error',
+      'import/no-named-as-default': 0,
+      'import/no-case-sensitive': 'error', // 👈 This is the key rule
+    },
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx'],
+        },
+      },
     },
   },
 ]
