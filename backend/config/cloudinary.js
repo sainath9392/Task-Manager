5c43b5c476f77d3abb808d44817f037b62bba2d5
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-26 12:13:01 +0530

    Refactor code structure for improved readability and maintainability

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,11 +1,12 @@
-import { v2 as cloudinary } from "cloudinary";
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
 
-const connectCloudinary = async () => {
-    cloudinary.config({
-        cloud_name: process.env.CLDN_NAME,
-        api_key: process.env.CLDN_API_KEY,
-        api_secret: process.env.CLDN_API_SECRET
-    })
+const connectCloudinary = () => {
+  cloudinary.config({
+    cloud_name: process.env.CLDN_NAME,
+    api_key: process.env.CLDN_API_KEY,
+    api_secret: process.env.CLDN_API_SECRET,
+  });
 };
 
-export default connectCloudinary
+module.exports = connectCloudinary;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const connectCloudinary = require("./config/cloudinary")
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
