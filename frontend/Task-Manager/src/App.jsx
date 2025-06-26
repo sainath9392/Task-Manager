2cd822f291db49c2a2d629d733f47736bda2a94a
sainath9392 <sainathduvvuri03@gmail.com>
2025-06-26 10:28:53 +0530

    refactor: simplify CORS middleware and restructure App component

--- frontend/Task-Manager/src/App.jsx
+++ frontend/Task-Manager/src/App.jsx
@@ -24,61 +24,56 @@ import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
-    <UserProvider>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<Login></Login>}></Route>
-            <Route path="/signup" element={<SignUp></SignUp>}></Route>
+    <>
+      <Routes>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
 
-            {/*Admin Routes*/}
-            <Route
-              element={<PrivateRoute allowedRoutes={["admin"]}></PrivateRoute>}
-            >
-              <Route path="/admin/dashboard" element={<Dashboard />}></Route>
-              <Route
-                path="/admin/tasks"
-                element={<ManageTasks></ManageTasks>}
-              ></Route>
-              <Route
-                path="/admin/create-task"
-                element={<CreateTask></CreateTask>}
-              ></Route>
-              <Route
-                path="/admin/users"
-                element={<ManageUsers></ManageUsers>}
-              ></Route>
-            </Route>
+        {/*Admin Routes*/}
+        <Route
+          element={<PrivateRoute allowedRoutes={["admin"]}></PrivateRoute>}
+        >
+          <Route path="/admin/dashboard" element={<Dashboard />}></Route>
+          <Route
+            path="/admin/tasks"
+            element={<ManageTasks></ManageTasks>}
+          ></Route>
+          <Route
+            path="/admin/create-task"
+            element={<CreateTask></CreateTask>}
+          ></Route>
+          <Route
+            path="/admin/users"
+            element={<ManageUsers></ManageUsers>}
+          ></Route>
+        </Route>
 
-            {/*User Routes*/}
-            <Route
-              element={<PrivateRoute allowedRoles={["admin"]}></PrivateRoute>}
-            >
-              <Route
-                path="/user/dashboard"
-                element={<UserDashboard></UserDashboard>}
-              ></Route>
-              <Route path="/user/tasks" element={<MyTasks></MyTasks>}></Route>
-              <Route
-                path="/user/tasks-details/:id"
-                element={<ViewTaskDetails></ViewTaskDetails>}
-              ></Route>
-            </Route>
+        {/*User Routes*/}
+        <Route element={<PrivateRoute allowedRoles={["admin"]}></PrivateRoute>}>
+          <Route
+            path="/user/dashboard"
+            element={<UserDashboard></UserDashboard>}
+          ></Route>
+          <Route path="/user/tasks" element={<MyTasks></MyTasks>}></Route>
+          <Route
+            path="/user/tasks-details/:id"
+            element={<ViewTaskDetails></ViewTaskDetails>}
+          ></Route>
+        </Route>
 
-            {/* Default Route */}
-            <Route path="/" element={<Root />}></Route>
-          </Routes>
-        </Router>
-      </div>
+        {/* Default Route */}
+        <Route path="/" element={<Root />}></Route>
+      </Routes>
 
-      <Toaster toastOptions={{
-        className:"",
-        style:{
-          fontSize:"13px",
-        },
-      }}
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
       />
-    </UserProvider>
+    </>
   );
 };
 
